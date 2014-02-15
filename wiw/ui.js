7251f8c5ac2896de9f54f66f01eca45d5434cdf6
alrocar <caldodeave@gmail.com>
2014-02-15 13:16:53 +0100

    finish dialog

--- wiw/ui.js
+++ wiw/ui.js
@@ -15,6 +15,14 @@ var UI = es.alrocar.UI = {
 };
 
 (function(UI) {
+    var finishTemplate = 
+        '<div id="finish-content">' +
+            '<h1 class="main-title">Game Finished!</h1>' +
+            '<h3 class="sub-title">Thanks for playing this beta version. More games coming soon...</h3>' +
+            '<div class="section">' +
+              '<h3></h3>' +
+            '</div>' +
+        '</div>';
     
     UI.ui = function() {
         var self = this;
@@ -41,6 +49,7 @@ var UI = es.alrocar.UI = {
 
         iniGameBar: function() {
             // $("#gamebar").hide();
+            this._removeFinishbar();
             var $gamebar = $("#gamebar");
             this.$gamebar = $gamebar;
             if (this.firstGame) {                
@@ -48,7 +57,7 @@ var UI = es.alrocar.UI = {
                 $gamebar.parent().append("<div class='saw-white saww'></div>");
                 $gamebar.addClass("gameback");
                 $gamebar.pointcounter({initialValue: 5});
-                $gamebar.downclock({initialValue: 12});
+                $gamebar.downclock({initialValue: 5});
                 $gamebar.append($("<div class='question'><label class='question-label' id='q'></label></div>"));
                 $gamebar.append($("<div class='question_time'></div>"));
                 $gamebar.css("width", 0);
@@ -89,41 +98,52 @@ var UI = es.alrocar.UI = {
         },
 
         showFinishGameBar: function(user) {
-            // var $gamebar = this.$gamebar.clone();
-            // if (this.firstGame) {                
-            //     $gamebar.parent().append("<div class='saw-black saww'></div>");
-            //     $gamebar.parent().append("<div class='saw-white saww'></div>");
-            //     $gamebar.addClass("gameback");
-            //     $gamebar.pointcounter({initialValue: 5});
-            //     $gamebar.downclock({initialValue: 12});
-            //     $gamebar.append($("<div class='question'><label class='question-label' id='q'></label></div>"));
-            //     $gamebar.append($("<div class='question_time'></div>"));
-            //     $gamebar.css("width", 0);
-            //     $(".saww").css("left", -50);
-            //     var gamebarPos = $('body').outerWidth() - $(".limiter").outerWidth() - $(".saw").outerWidth();
-            //     $(".saww").animate({"left":  gamebarPos}, 1000, function(){});
-            //     $gamebar.animate({opacity: 1,"width": gamebarPos}, 1000, 
-            //         function() {
-            //             self.game.onGameBarInited();                    
-            //         });
-            //     var self = this;
-            //     $gamebar.bind("secondpassed", function() {
-            //         self.game.updateTime();
-            //     });
-            //     $gamebar.bind("clockzero", function() {
-            //         setTimeout(function() {
-            //             self.game.gameOver();
-            //         }, 1000);
-            //     });
-            //     this.firstGame = false;
-            // } else {
-            //     $gamebar.downclock("start");
-            //     $gamebar.pointcounter("reset");
-            // }
+            var self = this;
+            var $gamebar = this.$gamebar.clone();
+            $gamebar.attr('id', 'finishbar');
+            $gamebar.find('#downclock').remove();
+            $gamebar.find('#pointcounter').remove();
+            $gamebar.find('.question').remove();
+            $gamebar.find('.question_time').remove();
+            
+            this.$finishbar = $gamebar;
+            
+            $gamebar.css("width", 0);
+            $('#gamebar-container').prepend($gamebar);
+
+            $gamebar.parent().append("<div class='saw-finish saww finish'></div>");
+            $gamebar.addClass("gameback").addClass('finish');
+            
+            $(".saww.finish").css("left", -50);            
+            
+            var fadeOutTime = 250;
+            self.$gamebar.find('#downclock').fadeOut(fadeOutTime);
+            self.$gamebar.find('#pointcounter').fadeOut(fadeOutTime);
+            self.$gamebar.find('.question').fadeOut(fadeOutTime);
+            self.$gamebar.find('.question_time').fadeOut(fadeOutTime);
+            
+            self.$gamebar.parent().find('.saw-black').fadeOut(fadeOutTime);
+            self.$gamebar.parent().find('.saw-white').fadeOut(fadeOutTime);
+
+            var gamebarPos = $('body').outerWidth() - $(".limiter").outerWidth() - $(".saw").outerWidth();
+            var animationDuration = 1000;
+            
+            self.$gamebar.animate({
+                left: gamebarPos,
+                opacity: 0.5
+            }, animationDuration);
+
+            $(".saww.finish").animate({"left":  gamebarPos}, animationDuration, function(){});
+            $gamebar.animate({opacity: 1,"width": gamebarPos}, animationDuration, 
+            function() {
+                self.$gamebar.fadeOut();
+                var $finishTemplate = $(finishTemplate).clone();
+                self.$finishbar.append($finishTemplate);
+            });
         },
 
         showScoreBoardDialog: function(user) {
-            alert("Your score: " + user.getCurrentScore());
+            console.log("IMPLEMENT THIS!!!");
         },
 
         setQuestion: function(question, timeForNextQuestion) {
@@ -172,6 +192,14 @@ var UI = es.alrocar.UI = {
 
         removeTime: function(seconds) {
             this.$gamebar.downclock("removeTime", seconds);  
+        },
+
+        _removeFinishbar: function() {
+            if (this.$finishbar) {
+                this.$finishbar.fadeOut(200, function() {
+                    $(this).remove();
+                });
+            }
         }
     };
 }
